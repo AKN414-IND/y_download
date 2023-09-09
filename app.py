1685865fdf91a299bf1494a798d2a1a516403fde
AKN414-IND <85106890+AKN414-IND@users.noreply.github.com>
2023-09-09 22:34:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask import Flask, request, send_file
 from pytube import YouTube
 import os
-
 app = Flask(__name__)
-
 @app.route('/download', methods=['POST'])
 def download():
     link = request.form.get('link')
